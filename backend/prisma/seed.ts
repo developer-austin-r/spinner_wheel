fa80faadda5c3e0713ad37cce4e82beb1b67f300
austinpentester <austinpentester@gmail.com>
2026-05-13 20:51:07 +0000

    feat: add SpinnerColor model and corresponding database seed script

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,12 +1,14 @@
 import { PrismaClient } from '@prisma/client';
 import { seedRoles } from './seeds/role.seed';
 import { seedUsers } from './seeds/user.seed';
+import { seedSpinnerColors } from './seeds/spinner-color.seed';
 
 const prisma = new PrismaClient();
 
 async function main() {
     await seedRoles(prisma);
     await seedUsers(prisma);
+    await seedSpinnerColors(prisma);
     console.log('All seeds completed.');
 }
 
